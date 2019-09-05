8dcc33fdc6acb4d0897b5447e4af8c000646e4d0
greyson <guoguishen@nightstation.cn>
2019-09-06 01:16:13 +0800

    little study from kotlin fun

--- app/src/main/java/com/testdemo/testjava/TestKotlin.kt
+++ app/src/main/java/com/testdemo/testjava/TestKotlin.kt
@@ -8,6 +8,13 @@ open class Test {
     }
 
     fun <R> myLet(sign: Int, block: (Int) -> R): R = block(sign)
+
+    /*fun canNoParam() {
+        println("canNoParam")
+    }*/
+    fun canNoParam(param: String = "yes") {
+        println("canNoParam param = $param")
+    }
 }
 
 class SunTest : Test() {
@@ -17,7 +24,7 @@ class SunTest : Test() {
 }
 //扩展方法
 fun Test.addMethod(param : String) {
-    println("addition receive param=" + param)
+    println("addition receive param = $param")
 }
 //扩展变量
 val SunTest.addition : String
@@ -40,15 +47,18 @@ fun main(args: Array<String>) {
     val s1 = "a is $a"
     a = 2
     val s2 = "${s1.replace("is", "was")}, but now is $a"
-    println(s2 + " - " + hasPrefix("prefixcc"))
+    println("$s2 - ${hasPrefix("prefixcc")}")
+
     foo()
-    println("Test's code = " + Test().getString())
+
+    val testInstance = Test()
+    println("Test's code = ${Test().getString()}\nTest's canNoParam invoke:")
+    testInstance.canNoParam()
 
     var test = SunTest()
     test.printThing()
     test.addMethod("addadd")
     println("扩展的变量：${test.addition}")
-
     println(test.myLet(111){ println("自己定义的Lambda表达式，参数为：$it"); return@myLet "no"})
 
     var testpp = 1
