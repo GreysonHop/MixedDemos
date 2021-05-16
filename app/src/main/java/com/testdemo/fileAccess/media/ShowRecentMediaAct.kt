9dbea30d039e134021a14483fdb9cd216fcf31d0
greyson <guishen.guo@voidtech.com.cn>
2021-05-16 22:59:10 +0800

    feat: 图片能正常读取，并从SQL语句上读取限定时间内

--- app/src/main/java/com/testdemo/fileAccess/media/ShowRecentMediaAct.kt
+++ app/src/main/java/com/testdemo/fileAccess/media/ShowRecentMediaAct.kt
@@ -1,12 +1,9 @@
 package com.testdemo.fileAccess.media
 
-import android.R
 import android.database.Cursor
 import android.os.Bundle
-import android.os.SystemClock
 import android.provider.MediaStore
 import android.util.Log
-import androidx.fragment.app.FragmentActivity
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
@@ -16,7 +13,6 @@ import com.testdemo.databinding.ActRecentMediaBinding
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
-
 
 /**
  * Create by Greyson on 2021/05/12
@@ -44,19 +40,37 @@ class ShowRecentMediaAct : BaseBindingActivity<ActRecentMediaBinding>() {
 
     override fun initView() {
         binding.btnShowPic.setOnClickListener {
-            // if (LoaderManager.getInstance(this).getLoader<Cursor>(ImageLoaderID) == null)
-            LoaderManager.getInstance(this).initLoader(ImageLoaderID, null, loaderCallback)
+            LoaderManager.getInstance(this).apply {
+                if (getLoader<Cursor>(ImageLoaderID) == null) {
+                    initLoader(ImageLoaderID, null, loaderCallback)
+                } else {
+                    restartLoader(ImageLoaderID, null, loaderCallback)
+                    // getLoader<CursorLoader>(ImageLoaderID)?.reset()
+                }
+            }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        LoaderManager.getInstance(this).destroyLoader(ImageLoaderID)
     }
 
     private val loaderCallback = object : LoaderManager.LoaderCallbacks<Cursor> {
         override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
-            var cursorLoader: CursorLoader? = null
+            //TODO greyson_2021/5/16 为了测试方便，先把时间调成72小时内
+            val timeLimit = System.currentTimeMillis() - 72 * 60 * 60 * 1000 // 一分钟前
+
+            Log.d("greyson", "onCreateLoader: id=$id")
+            val cursorLoader: CursorLoader?
             cursorLoader = CursorLoader(
                 this@ShowRecentMediaAct,
-                if (id == ImageLoaderID) MediaStore.Images.Media.EXTERNAL_CONTENT_URI else MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                if (id == ImageLoaderID)
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+                else
+                    MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
                 IMAGE_PROJECTION,
-                null/*"${MediaStore.Images.ImageColumns.DATE_TAKEN} < "*/,
+                /*null*/"${MediaStore.Images.ImageColumns.DATE_TAKEN} > $timeLimit",
                 null,
                 IMAGE_PROJECTION[7] + " DESC"
             )
@@ -66,12 +80,11 @@ class ShowRecentMediaAct : BaseBindingActivity<ActRecentMediaBinding>() {
         override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
             if (data == null) return
 
-            val allImages: ArrayList<LocalMedia> = ArrayList() //所有图片的集合,不分文件夹
+            val allImages: ArrayList<LocalMedia> = ArrayList() // 所有图片的集合,不分文件夹
             val currentTime: Long = System.currentTimeMillis()
 
             while (data.moveToNext()) {
 
-                //查询数据
                 val imageName: String = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[0]))
                 val imagePath: String = data.getString(data.getColumnIndexOrThrow(IMAGE_PROJECTION[1]))
                 val imageSize: Long = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[2]))
@@ -82,28 +95,30 @@ class ShowRecentMediaAct : BaseBindingActivity<ActRecentMediaBinding>() {
                 val imageTakenTime: Long = data.getLong(data.getColumnIndexOrThrow(IMAGE_PROJECTION[7]))
                 val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.CHINA)
 
-                Log.d("TAG", "onLoadFinished 遍历图片列表: \n\t$imageName, takeTime=${dateFormat.format(imageTakenTime)}" +
+                Log.d("greyson", "onLoadFinished 遍历图片列表: \n\t$imageName, takeTime=${dateFormat.format(imageTakenTime)}" +
                         ", \n\taddTime=${dateFormat.format(imageAddTime)} _ ${imageAddTime / 1000}"
                         + ", \n\tsize=$imageSize, $imageMimeType")
 
 
-                if (currentTime - imageTakenTime < 60 * 1000) {
+                // if (currentTime - imageTakenTime < 60 * 1000) {
 
-                    val imageItem = LocalMedia()
-                    // imageItem.setFolderName(imageName)
-                    imageItem.setPath(imagePath)
-                    // imageItem.si = imageSize
-                    imageItem.setWidth(imageWidth)
-                    imageItem.setHeight(imageHeight)
-                    // imageItem.setMime(imageMimeType)
-                    // imageItem.setAddTime(imageAddTime)
-                    allImages.add(imageItem)
-                }
+                val imageItem = LocalMedia()
+                // imageItem.setFolderName(imageName)
+                imageItem.setPath(imagePath)
+                // imageItem.si = imageSize
+                imageItem.setWidth(imageWidth)
+                imageItem.setHeight(imageHeight)
+                // imageItem.setMime(imageMimeType)
+                // imageItem.setAddTime(imageAddTime)
+                allImages.add(imageItem)
+                // }
             }
-            Log.d("TAG", "onLoadFinished 最终的结果列表: " + allImages.size)
+            Log.d("greyson", "onLoadFinished 最终的结果列表: " + allImages.size)
         }
 
         override fun onLoaderReset(loader: Loader<Cursor>) {
+            // Loader被reset时调用，它所读取的数据要取消引用
+            Log.d("greyson", "onLoaderReset: ${loader.id}")
         }
     }
 }
