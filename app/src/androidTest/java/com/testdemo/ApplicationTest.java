eb4bc3c6f0fba8e875a8960c9d7506b3b52ee41c
guishen <guishen.guo@voidtech.com.cn>
2021-08-02 18:17:12 +0800

    feat: 安卓10文件读取测试

--- app/src/androidTest/java/com/testdemo/ApplicationTest.java
+++ app/src/androidTest/java/com/testdemo/ApplicationTest.java
@@ -3,7 +3,10 @@ package com.testdemo;
 import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.DocumentsProvider;
 import android.util.Base64;
 import android.util.Log;
 import android.widget.ImageView;
@@ -15,11 +18,17 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
- *
+ * <p>
  * See [testing documentation](http://d.android.com/tools/testing).
  *
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
@@ -76,5 +85,50 @@ public class ApplicationTest {
         Log.d("greyson", new String(bytes4));
         Log.d("greyson", new String(bytes5));
         Log.d("greyson", new String(bytes6));
+    }
+
+    @Test
+    public void testAndroidQ() {
+        BufferedInputStream bufferedInputStream = null;
+        try {
+            File file = mTargetContext.getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+            Log.d("greyson", "Documents' path: " + file.getAbsolutePath());
+            final String fileName = "test.txt";
+            // final String filePath = file.getAbsolutePath() + "/" + fileName;
+            final String filePath = "/storage/emulated/0/Documents/" + fileName;
+            Log.d("greyson", "filePath's path: " + filePath);
+            final String uriPath = "content://com.android.externalstorage.documents/document/primary%3ADocuments%2Ftest.txt";
+
+Uri uri1 = Uri.parse(uriPath);
+// Uri uri1 = Uri.parse("file:///sdcard/Documents/test.txt");
+String uri2 = Uri.decode("file:///sdcard/Documents/test.txt");
+            Log.d("greyson", "testAndroidQ: " + uri2);
+            // Uri uri = Uri.fromFile(new File(filePath));
+
+            InputStream is = mTargetContext.getContentResolver().openInputStream(uri1);
+            // InputStream is = mTargetContext.openFileInput(filePath);
+
+            bufferedInputStream = new BufferedInputStream(is);
+            Scanner scanner = new Scanner(bufferedInputStream);
+            while (scanner.hasNextLine()) {
+                Log.d("greyson", "content: " + scanner.nextLine());
+            }
+
+            /*byte[] buff = new byte[1024];
+            int buffLength = 0;
+            while ((buffLength = bufferedInputStream.read(buff)) > 0) {
+
+            }*/
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (bufferedInputStream != null) {
+                    bufferedInputStream.close();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
